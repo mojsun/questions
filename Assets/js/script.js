a940588fdfbee7d589e46142526cbd0949f60f53
mojsun <m.khorashahi7@gmail.com>
2023-01-23 20:13:53 -0500

    working1

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,7 +2,7 @@ let boxes = [
   {
     question: "commonly used data types do not include:",
     answers: ["1.string", "2.booleans", "3.alerts", "4.numbers"],
-    result: ["correct", "wrong!"],
+    result: ["wrong!"],
   },
   {
     question: "The condition in an if/else statment is enclosed with --------",
@@ -12,7 +12,7 @@ let boxes = [
       "3.parenthsis",
       "4.square brackets",
     ],
-    result: ["correct", "wrong!"],
+    result: ["correct"],
   },
   {
     question: "arrays in Java script can be used to store -------",
@@ -22,13 +22,13 @@ let boxes = [
       "3.booleans",
       "4.all of the above",
     ],
-    result: ["correct", "wrong!"],
+    result: ["correct"],
   },
   {
     question:
       "string values must be enclosed within -------- when being assigned to variables ",
     answers: ["1.commas", "2.curly bracket", "3.quotes", "4.patenthesis"],
-    result: ["correct", "wrong!"],
+    result: ["wrong!"],
   },
   {
     question:
@@ -39,9 +39,10 @@ let boxes = [
       "3.for loops",
       "4.console.log",
     ],
-    result: ["correct", "wrong!"],
+    result: ["wrong!"],
   },
 ];
+
 let currentQuestionIndex = 0;
 function showQuestion() {
   document.getElementById("question").textContent =
@@ -55,22 +56,45 @@ function showQuestion() {
   document.getElementById("optionFour").textContent =
     boxes[currentQuestionIndex].answers[3];
 }
+
 function checkAnswer() {
   currentQuestionIndex++;
   showQuestion();
+  //setTime();
+  /*document.getElementById("result").textContent =
+    boxes[currentQuestionIndex].result[0];
+  checkAnswer();*/
 }
+/*var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("timer");
+var secondleft = 75;
+function setTime() {
+  var timerInterval = setinterval(function () {
+    secondleft--;
+    if (secondleft === 0) {
+      clearInterval(timerInterval);
+    }
+    1000;
+  });*/
+
 let button = document.querySelector("#firstButton");
 
 button.addEventListener("click", function () {
   document.getElementById("first").setAttribute("class", "hide");
   document.getElementById("second").setAttribute("class", "");
   showQuestion();
+  //setTime();
 });
+/*let lastPage = boxes[4].answers[2];
+lastPage.addEventListener("click", function () {
+  document.getElementById("second").setAttribute("class", "hide");
+  document.getElementById("third").setAttribute("class", "");
+});*/
 
-let lastAnswer = document.querySelector("");
-
+/*function showLastPage() {
+  document.getElementById("third");
+}*/
 document.getElementById("optionOne").addEventListener("click", checkAnswer);
-
 document.getElementById("optionTwo").addEventListener("click", checkAnswer);
 document.getElementById("optionThree").addEventListener("click", checkAnswer);
 document.getElementById("optionFour").addEventListener("click", checkAnswer);
