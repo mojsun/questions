7a0b4f86c06547f8acfc37ba541ea2d4ddbf8785
mojsun <m.khorashahi7@gmail.com>
2023-01-24 17:51:19 -0500

    working

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -2,17 +2,17 @@ let boxes = [
   {
     question: "commonly used data types do not include:",
     answers: ["1.string", "2.booleans", "3.alerts", "4.numbers"],
-    result: ["wrong!"],
+    result: "3.alerts",
   },
   {
     question: "The condition in an if/else statment is enclosed with --------",
     answers: [
       "1.quotes",
       "2.curly brackets",
-      "3.parenthsis",
+      "3.parenthesis",
       "4.square brackets",
     ],
-    result: ["correct"],
+    result: "3.parenthesis",
   },
   {
     question: "arrays in Java script can be used to store -------",
@@ -57,43 +57,53 @@ function showQuestion() {
     boxes[currentQuestionIndex].answers[3];
 }
 
-function checkAnswer() {
+function checkAnswer(event) {
+  var userChoice = event.target.textContent;
+  if (userChoice == boxes[currentQuestionIndex].result) {
+    console.log("correct");
+    document.getElementById("result").setAttribute("class", "");
+    document.getElementById("result").textContent = "correct";
+  } else {
+    console.log("here");
+    document.getElementById("result").setAttribute("class", "");
+    document.getElementById("result").textContent = "wrong!";
+  }
+
+  setTimeout(function () {
+    document.getElementById("result").setAttribute("class", "hide");
+    if (currentQuestionIndex < boxes.length) {
+      showQuestion();
+    }
+  }, 1500);
   currentQuestionIndex++;
-  showQuestion();
-  //setTime();
-  /*document.getElementById("result").textContent =
-    boxes[currentQuestionIndex].result[0];
-  checkAnswer();*/
 }
-/*var timeEl = document.querySelector(".time");
+
+function endQuiz() {
+  document.getElementById("second").setAttribute("class", "hide");
+  document.getElementById("third").setAttribute("class", "");
+}
+var timeEl = document.querySelector(".time");
 var mainEl = document.getElementById("timer");
 var secondleft = 75;
 function setTime() {
-  var timerInterval = setinterval(function () {
+  var timerInterval = setInterval(function () {
     secondleft--;
-    if (secondleft === 0) {
+    timeEl.textContent = secondleft;
+    if (secondleft <= 0 || currentQuestionIndex >= boxes.length) {
       clearInterval(timerInterval);
+      endQuiz();
     }
-    1000;
-  });*/
-
+  }, 1000);
+}
 let button = document.querySelector("#firstButton");
 
 button.addEventListener("click", function () {
+  setTime();
   document.getElementById("first").setAttribute("class", "hide");
   document.getElementById("second").setAttribute("class", "");
   showQuestion();
-  //setTime();
 });
-/*let lastPage = boxes[4].answers[2];
-lastPage.addEventListener("click", function () {
-  document.getElementById("second").setAttribute("class", "hide");
-  document.getElementById("third").setAttribute("class", "");
-});*/
 
-/*function showLastPage() {
-  document.getElementById("third");
-}*/
 document.getElementById("optionOne").addEventListener("click", checkAnswer);
 document.getElementById("optionTwo").addEventListener("click", checkAnswer);
 document.getElementById("optionThree").addEventListener("click", checkAnswer);
